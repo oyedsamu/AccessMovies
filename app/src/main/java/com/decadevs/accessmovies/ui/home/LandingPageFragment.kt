d8596f241bd6fccacd951e98b32b6ab7f741ed6f
Daniel Ayodeji <danielayodeji97@gmail.com>
2020-11-09 03:38:38 +0100

    app recheck

--- app/src/main/java/com/decadevs/accessmovies/ui/home/LandingPageFragment.kt
+++ app/src/main/java/com/decadevs/accessmovies/ui/home/LandingPageFragment.kt
@@ -41,9 +41,9 @@ class LandingPageFragment : Fragment(R.layout.fragment_landing_page), OnItemClic
 
 
 
-        binding.testing.setOnClickListener {
-            findNavController().navigate(R.id.addMovieFragment)
-        }
+//        binding.testing.setOnClickListener {
+//            findNavController().navigate(R.id.addMovieFragment)
+//        }
 
 
     }
